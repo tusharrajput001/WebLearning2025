cc67b0ba8ae17717d623b4089e40b635fdd7275b
Tushar rajput <tusharr0491@gmail.com>
2025-05-02 18:32:14 +0530

    es6concepts refreshing

---
+++ Phase 1 JavaScript Refresher/Day2/JsEsconceps.js
@@ -0,0 +1,36 @@
+// callbacks
+// when a function call another function from parameters is callback
+
+// function myDisplayer(some){
+//     document.getElementById("demo").innerHTML = some;
+// }
+
+// function myCalculator(n1,n2, myCallback){
+//     let sum = n1 + n2;
+//     myCallback(sum);
+// }
+
+// myCalculator(2,3,myDisplayer);
+
+// //another ex
+
+// const myNumbers = [4, 1, -20, -7, 5, 9, -6];
+
+// const posNum = removeNeg(myNumbers, (x)=> x >= 0);
+
+// function removeNeg(numbers, callback){
+//     const myArr = [];
+//     for(const x of numbers){
+//         if(callback(x)){
+//             myArr.push(x);
+//         }
+//     }
+//     return myArr;
+// }
+
+//Promise
+
+
+
+
+

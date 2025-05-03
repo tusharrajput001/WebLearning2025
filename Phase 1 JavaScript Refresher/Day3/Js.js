6ae85936cc4ea11091506eb3ff62c1ba0d77c6f5
Tushar rajput <tusharr0491@gmail.com>
2025-05-03 22:33:03 +0530

    Light Mode nd Dark mode

--- Phase 1 JavaScript Refresher/Day3/Js.js
+++ Phase 1 JavaScript Refresher/Day3/Js.js
@@ -3,18 +3,15 @@
 
 // let heading = document.getElementById("heading").innerHTML = "js Magic"
 
-
 // document.querySelector("button").onclick = function(){
 //     alert("Clicked");
 // }
-
 
 // document.querySelector("p").onmouseover = () => {
 //     document.getElementById("para").innerHTML = "GoodBye!!"
 // }
 
-
-// const bg = document.getElementsByTagName("body")[0];
+const bg = document.getElementsByTagName("body")[0];
 // const red = document.getElementById("Btn-Red");
 // const blue = document.getElementById("Btn-Blue");
 // const green = document.getElementById("Btn-Green");
@@ -47,29 +44,36 @@
 //     console.log(storedData);
 // }
 
-
-document.getElementById("myForm").addEventListener("click" , (e) => {
-    e.preventDefault();
-})
-let submitBtn = document.querySelector("#submit")
+document.getElementById("myForm").addEventListener("click", (e) => {
+  e.preventDefault();
+});
+let submitBtn = document.querySelector("#submit");
 
 submitBtn.onclick = () => {
-    const fname = document.getElementById("Fname").value
-    const lname = document.getElementById("Lname").value
-    const email = document.getElementById("Email").value
-    const phone = document.getElementById("Phone").value
+  const fname = document.getElementById("Fname").value;
+  const lname = document.getElementById("Lname").value;
+  const email = document.getElementById("Email").value;
+  const phone = document.getElementById("Phone").value;
 
-    let payload = {
-        firstname : fname,
-        lastname : lname,
-        email_id : email,
-        phone_no : phone
-    }
-    
-    localStorage.setItem("formData", JSON.stringify(payload));
+  let payload = {
+    firstname: fname,
+    lastname: lname,
+    email_id: email,
+    phone_no: phone,
+  };
 
-    const storedData = JSON.parse(localStorage.getItem("formData"))
-    console.log(storedData);
+  localStorage.setItem("formData", JSON.stringify(payload));
 
-}
+  const storedData = JSON.parse(localStorage.getItem("formData"));
+  console.log(storedData);
+};
 
+document.querySelector("#SwitchBtn").onclick = () => {
+  if (bg.style.backgroundColor === "black") {
+    bg.style.backgroundColor = "white";
+    document.getElementById("SwitchBtn").innerHTML = "Light Mode";
+  } else {
+    bg.style.backgroundColor = "black";
+    document.getElementById("SwitchBtn").innerHTML = "Dark Mode";
+  }
+};

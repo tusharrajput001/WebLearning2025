04b468ea312e7fc5d219e37dcd59ff4202d1c35e
Tushar rajput <tusharr0491@gmail.com>
2025-05-10 14:03:01 +0530

    changes fix

--- Phase 3 Intermediate React/Day5/src/api/fetchWeather.js
+++ Phase 3 Intermediate React/Day5/src/api/fetchWeather.js
@@ -3,7 +3,10 @@ export async function fetchWeather(city) {
   const response = await fetch(
     `https://api.weatherstack.com/current?access_key=${API_KEY}&query=${city}`
   );
-  console.log("Fetching from URL:", `https://api.weatherstack.com/current?access_key=${API_KEY}&query=${city}`);
+  console.log(
+    "Fetching from URL:",
+    `https://api.weatherstack.com/current?access_key=${API_KEY}&query=${city}`
+  );
 
   if (!response.ok) {
     throw new Error("City not found");

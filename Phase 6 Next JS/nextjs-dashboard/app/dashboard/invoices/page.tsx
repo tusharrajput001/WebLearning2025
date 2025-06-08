feff39916c7074d1c8ed60c76ef5bcbf119a94d5
Tushar rajput <tusharr0491@gmail.com>
2025-06-08 15:21:10 +0530

    App Router

---
+++ Phase 6 Next JS/nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function InvoicesPage() {
+  return (
+    <div>Invoices Page</div>
+  )
+}
+
+export default InvoicesPage
---
+++ Phase 6 Next JS/nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>Dashboard page</div>
+  )
+}
+
+export default Page
